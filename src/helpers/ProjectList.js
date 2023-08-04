bd1e85d5c28c90781b4db2ce1541da69a32a0eb3
mansoor <mansoorshaik7b@gmail.com>
2023-08-04 08:39:09 +0530

    added a new project- Travel Website

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,6 +1,7 @@
 
 import MovieDB from "../images/MovieDB.png"
 import Reciepeapp from '../images/Recipeapp.png'
+import Travel from '../images/Travel.png'
 
 export const projectList = [
     {
@@ -12,6 +13,11 @@ export const projectList = [
         image: Reciepeapp,
         projectname: "Reciepe Searcher",
         link: 'https://mansoorshaik02.github.io/Recipe-Website/'
+    },
+    {
+        image: Travel,
+        projectname: "Travel Website",
+        link: 'https://mansoorshaik02.github.io/React-travel-website/'
     }
 
 ]
