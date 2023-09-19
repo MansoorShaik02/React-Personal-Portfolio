851dc9cfa0c235affec9d60bac283d935a2424d8
mansoor <mansoorshaik7b@gmail.com>
2023-09-19 13:10:30 +0530

    added React Movie Project to it

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -2,6 +2,7 @@
 import MovieDB from "../images/MovieDB.png"
 import Reciepeapp from '../images/Recipeapp.png'
 import Travel from '../images/Travel.png'
+import ReactMovieWebsite from '../images/ReactMovieWebsite.png'
 
 export const projectList = [
     {
@@ -17,6 +18,11 @@ export const projectList = [
     {
         image: Travel,
         projectname: "Travel Website",
+        link: 'https://mansoorshaik02.github.io/React-travel-website/'
+    },
+    {
+        image: ReactMovieWebsite,
+        projectname: "React Movie Website",
         link: 'https://mansoorshaik02.github.io/React-travel-website/'
     }
 
